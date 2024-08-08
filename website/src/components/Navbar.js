294688867754d815632a76d1c49141cc60e02af0
Gupta955 <guptasurbhi483@gmail.com>
2024-08-08 12:52:57 +0530

    nav bar linking change

--- website/src/components/Navbar.js
+++ website/src/components/Navbar.js
@@ -94,11 +94,11 @@ const Navbar = () => {
           {isServicesMenuOpen && (
             <div className={styles.dropdownContent}>
               <Link href="../../services" className={styles.dropdownContentLinks}>All Services</Link>
-              <Link href="../../services/hearing-test-in-delhi" className={styles.dropdownContentLinks}>Hearing Test Services</Link>
-              <Link href="../../services/speech-therapy-in-delhi" className={styles.dropdownContentLinks}>Speech Therapy Services</Link>
+              <Link href="../../services/hearing-test-delhi" className={styles.dropdownContentLinks}>Hearing Test Services</Link>
+              <Link href="../../services/speech-therapy-delhi" className={styles.dropdownContentLinks}>Speech Therapy Services</Link>
               {/* <Link href="../../services/Audiologist" className={styles.dropdownContentLinks}>Audiologist</Link> */}
-              <Link href="../../services/hearing-care-solution-in-delhi" className={styles.dropdownContentLinks}>Hearing Care</Link>
-              <Link href="../../services/hearing-aid-center-in-delhi" className={styles.dropdownContentLinks}>Hearing Aid Center</Link>
+              <Link href="../../services/hearing-care-solution-delhi" className={styles.dropdownContentLinks}>Hearing Care</Link>
+              <Link href="../../services/hearing-aid-center-delhi" className={styles.dropdownContentLinks}>Hearing Aid Center</Link>
             </div>
           )}
         </div>
